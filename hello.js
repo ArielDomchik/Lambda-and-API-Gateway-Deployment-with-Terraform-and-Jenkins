8d382febc62f82f304ee9c50e07b833c0a2cd000
ariel.domchik <ariyonzor@gmail.com>
2023-03-01 08:43:08 +0200

    Add project files

---
+++ hello.js
@@ -0,0 +1,10 @@
+exports.handler = function (event, context, callback) {
+  var response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+    body: '<h2>Hello world...Welcome to HELL...</h2>',
+  }
+  callback(null, response)
+}
