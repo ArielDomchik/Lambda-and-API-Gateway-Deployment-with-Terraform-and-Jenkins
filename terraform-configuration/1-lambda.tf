e9fd43d27d72f0984af53c5592b532288ae1a9c1
ariel.domchik <ariyonzor@gmail.com>
2023-03-02 14:05:23 +0200

    Changed terraform configuration file, added local variable of path of zip file and base64 to include it in lambda function

--- terraform-configuration/1-lambda.tf
+++ terraform-configuration/1-lambda.tf
@@ -1,6 +1,6 @@
 locals {
   file_path = "/home/ubuntu/workspace/Lambda/src/hello.zip"
-  file_base64sha256 = filesha256("${local.file_path}")
+  file_base64sha256 = filesha256("/home/ubuntu/workspace/Lambda/src/hello.zip")
 }
 
 resource "aws_lambda_function" "myLambda" {
