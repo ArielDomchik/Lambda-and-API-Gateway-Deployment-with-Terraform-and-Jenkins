eda23ccc46809f6c1f87124828fa38670a332e79
ariel.domchik <ariyonzor@gmail.com>
2023-03-02 11:40:46 +0200

    Testing app

--- src/hello.js
+++ src/hello.js
@@ -4,7 +4,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<h2>Hello world...Welcome to LEUMI...</h2>',
+    body: '<h2>Hello world...Welcome to LEUMI...!</h2>',
   }
   callback(null, response)
 }
